5fc97065dcc049f3e59bf61ae5c403f64a0d5100
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-07-03 18:56:29 -0300

    change src links for images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,22 +1,34 @@
 import React from "react";
+import html from "/imgs/icons/html.png";
+import css from "/imgs/icons/css.png";
+import js from "/imgs/icons/js.png";
+import react from "/imgs/icons/react.png";
+import redux from "/imgs/icons/redux.png";
+import node from "/imgs/icons/node.png";
+import express from "/imgs/icons/express.png";
+import sequelize from "/imgs/icons/sequelize.png";
+import postgre from "/imgs/icons/postgre.png";
+import github from "/imgs/icons/github.png";
+import slack from "/imgs/icons/slack.png";
+import dc from "/imgs/icons/dc.png";
 
 const Skills = () => {
   return (
     <section id="tech" className="skills container">
       <h2>Tecnologías y herramientas</h2>
       <div className="skills-content">
-        <img src="https://cdn-icons-png.flaticon.com/512/732/732212.png" />
-        <img src="https://cdn-icons-png.flaticon.com/512/732/732190.png" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png" />
-        <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" />
-        <img src="https://assets.stickpng.com/images/5848309bcef1014c0b5e4a9a.png" />
-        <img src="https://cdn.freebiesupply.com/logos/thumbs/2x/nodejs-1-logo.png" />
-        <img src="https://w7.pngwing.com/pngs/545/451/png-transparent-node-js-express-js-javascript-solution-stack-web-application-others-angle-text-rectangle-thumbnail.png" />
-        <img src="https://seeklogo.com/images/S/sequelize-logo-9A5075DB9F-seeklogo.com.png" />
-        <img src="https://cdn-icons-png.flaticon.com/512/5968/5968342.png" />
-        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/2048px-Slack_icon_2019.svg.png" />
-        <img src="https://www.freepnglogos.com/uploads/discord-logo-png/concours-discord-cartes-voeux-fortnite-france-6.png" />
+        <img src={html} alt="html" />
+        <img src={css} />
+        <img src={js} />
+        <img src={react} />
+        <img src={redux} />
+        <img src={node} />
+        <img src={express} />
+        <img src={sequelize} />
+        <img src={postgre} />
+        <img src={github} />
+        <img src={slack} />
+        <img src={dc} />
       </div>
     </section>
   );
