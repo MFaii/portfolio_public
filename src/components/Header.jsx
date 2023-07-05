12792241dd844f0c8e19a47fe2f6fc1b8a0b3f90
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-07-05 17:57:25 -0300

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,12 @@ import CV from "/Maximiliano Failla CV.pdf";
 const Header = () => {
   return (
     <header className="header">
-      <div className="menu container animate__animated animate__fadeInDownBig">
+      <div className="menu container">
         <a href="#" className="logo" id="home">
           {`{MF}`}
         </a>
         <input type="checkbox" id="menu" />
-        <label for="menu">
+        <label htmlFor="menu">
           <img
             src="https://static.vecteezy.com/system/resources/previews/009/392/001/non_2x/app-menu-icon-sign-symbol-design-free-png.png"
             className="menu-icono"
@@ -38,11 +38,9 @@ const Header = () => {
       </div>
       <div className="header-content container">
         <div className="header-txt">
-          <span className="animate__animated animate__bounceInDown">Soy</span>
-          <h1 className="animate__animated animate__bounceInLeft">
-            Desarrollador Web Full Stack
-          </h1>
-          <div className="butons animate__animated animate__fadeInBottomRight">
+          <span className="animation">Soy</span>
+          <h1 className="animation">Desarrollador Web Full Stack</h1>
+          <div className="butons animation">
             <a href={CV} className="btn-1" download target="_blank">
               CV - Descargar
             </a>
